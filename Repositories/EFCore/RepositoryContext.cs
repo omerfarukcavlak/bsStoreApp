5337f5c1d4471c7ee54746a2dab619f43200b144
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2025-03-07 01:27:15 +0300

    Add project files.

---
+++ Repositories/EFCore/RepositoryContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Repositories.EFCore.Config;
+
+namespace Repositories.EFCore
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BookConfig());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Book> Books { get; set; }
+    
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,37 @@
+using NLog;
+using WebApi.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Presentation.AssemblyRefence).Assembly)
+    .AddNewtonsoftJson();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureLoggerService();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
